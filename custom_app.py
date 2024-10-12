985e818916907801db55c854f8881734934ec5d3
AK391 <ahsen@huggingface.co>
2024-10-12 15:37:59 -0400

    updates

--- custom_app.py
+++ custom_app.py
@@ -1,10 +1,9 @@
 import gradio as gr
-import openai_gradio
+from langchain_gradio import registry
 
-gr.load(
+registry(
     name='gpt-4-turbo',
-    src=openai_gradio.registry,
-    title='OpenAI-Gradio Integration',
+    title='LangChain-Gradio Integration',
     description="Chat with gpt-4-turbo model.",
     examples=["Explain quantum gravity to a 5-year old.", "How many R are there in the word Strawberry?"]
 ).launch()
